3ff95bea57e45898055c0eba3f0ccbd1f44e9a62
Conner Fromknecht <conner@mit.edu>
2018-01-16 00:52:39 -0800

    peer: init link with batched sphinx processing

--- peer.go
+++ peer.go
@@ -381,6 +381,7 @@ func (p *peer) loadActiveChannels(chans []*channeldb.OpenChannel) error {
 		linkCfg := htlcswitch.ChannelLinkConfig{
 			Peer:                  p,
 			DecodeHopIterator:     p.server.sphinx.DecodeHopIterator,
+			DecodeHopIterators:    p.server.sphinx.DecodeHopIterators,
 			DecodeOnionObfuscator: p.server.sphinx.ExtractErrorEncrypter,
 			GetLastChannelUpdate: createGetLastUpdate(p.server.chanRouter,
 				p.PubKey(), lnChan.ShortChanID()),
@@ -1274,6 +1275,7 @@ out:
 			linkConfig := htlcswitch.ChannelLinkConfig{
 				Peer:                  p,
 				DecodeHopIterator:     p.server.sphinx.DecodeHopIterator,
+				DecodeHopIterators:    p.server.sphinx.DecodeHopIterators,
 				DecodeOnionObfuscator: p.server.sphinx.ExtractErrorEncrypter,
 				GetLastChannelUpdate: createGetLastUpdate(p.server.chanRouter,
 					p.PubKey(), newChanReq.channel.ShortChanID()),
