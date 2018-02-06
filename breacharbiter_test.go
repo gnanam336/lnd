f722ba6559001fc3096c062d2785b87c7cbdeb00
Conner Fromknecht <conner@mit.edu>
2018-02-05 20:12:03 -0800

    fixup! breacharbiter_test: use new ReceiveRevocation signature

--- breacharbiter_test.go
+++ breacharbiter_test.go
@@ -1513,7 +1513,7 @@ func forceStateTransition(chanA, chanB *lnwallet.LightningChannel) error {
 		return err
 	}
 
-	if _, _, err := chanA.ReceiveRevocation(bobRevocation); err != nil {
+	if _, _, _, err := chanA.ReceiveRevocation(bobRevocation); err != nil {
 		return err
 	}
 	if err := chanA.ReceiveNewCommitment(bobSig, bobHtlcSigs); err != nil {
@@ -1524,7 +1524,7 @@ func forceStateTransition(chanA, chanB *lnwallet.LightningChannel) error {
 	if err != nil {
 		return err
 	}
-	if _, _, err := chanB.ReceiveRevocation(aliceRevocation); err != nil {
+	if _, _, _, err := chanB.ReceiveRevocation(aliceRevocation); err != nil {
 		return err
 	}
 
